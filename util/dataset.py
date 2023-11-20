0b96d37046886515568d49d6c1ef5d4684af6909
luoxiaoliu <150107163+luoxiaoliu@users.noreply.github.com>
2023-11-20 16:30:08 +0800

    Add files via upload

--- util/dataset.py
+++ util/dataset.py
@@ -30,100 +30,46 @@ def make_dataset(split=0, data_root=None, data_list=None, sub_list=None):
     # which means the mask will be downsampled to 1/32 of the original size and the valid area should be larger than 2, 
     # therefore the area in original size should be accordingly larger than 2 * 32 * 32    
     image_label_list = []  
-    list_read = open(data_list).readlines()#验证集总数1449.coco训练82081
+    list_read = open(data_list).readlines()
     print("Processing data...".format(sub_list))
     sub_class_file_list = {}
-    for sub_c in sub_list:#训练集[6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]测试集 [1, 2, 3, 4, 5]
+    for sub_c in sub_list:
         sub_class_file_list[sub_c] = []
-    if len(sub_list)>5:
-        for l_idx in tqdm(range(len(list_read))):  # 5953张图片
-            #forpascalvoc
-            line = list_read[l_idx]  # 'JPEGImages/2008_000008.jpg SegmentationClassAug/2008_000008.png'
-            line = line.strip()  #
-            line_split = line.split(' ')
-            image_name = os.path.join(data_root, line_split[
-                0])  # '/home/prlab/willow/Oneshot/data/VOCdevkit2012/VOC2012/JPEGImages/2008_000008.jpg'
-            label_name = os.path.join(data_root, line_split[
-                1])  # '/home/prlab/willow/Oneshot/data/VOCdevkit2012/VOC2012/SegmentationClassAug/2008_000008.png'
-            # line = list_read[l_idx]  # 'JPEGImages/2008_000008.jpg SegmentationClassAug/2008_000008.png'
-            # line = line.strip()  #
-            # line_split = line.split(' ')
-            # image_name = os.path.join(data_root,'images/train2014',line_split[
-            #     0][-29:])  # '/home/prlab/willow/Oneshot/data/VOCdevkit2012/VOC2012/JPEGImages/2008_000008.jpg'
-            # label_name = os.path.join(data_root, 'annotations/train2014',line_split[
-            #     1][-29:])  # '/home/prlab/willow/Oneshot/data/VOCdevkit2012/VOC2012/SegmentationClassAug/2008_000008.png'
 
-            item = (image_name, label_name)
-            label = cv2.imread(label_name, cv2.IMREAD_GRAYSCALE)  # (442, 500)
-            label_class = np.unique(label).tolist()  # [0, 13, 15]
+    for l_idx in tqdm(range(len(list_read))):
+        line = list_read[l_idx]
+        line = line.strip()
+        line_split = line.split(' ')
+        image_name = os.path.join(data_root, line_split[0])
+        label_name = os.path.join(data_root, line_split[1])
+        item = (image_name, label_name)
+        label = cv2.imread(label_name, cv2.IMREAD_GRAYSCALE)
+        label_class = np.unique(label).tolist()
 
-            if 0 in label_class:  # [0, 13, 15]
-                label_class.remove(0)  # [13, 15],去掉背景
-            if 255 in label_class:
-                label_class.remove(255)  # [13, 15]
-            # 先剔除小目标
-            new_label_class = []
-            for c in label_class:  # [13, 15]
-                if c in sub_list:  ##[6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-                    tmp_label = np.zeros_like(label)  # (442, 500)
-                    target_pix = np.where(label == c)  # 找到对应类的位置信息
-                    tmp_label[target_pix[0], target_pix[1]] = 1  # 将对应位置设置为1
-                    if tmp_label.sum() >= 2 * 32 * 32:  # 判断总的个数是否满足条件,相当于排除小目标
-                        new_label_class.append(c)
+        if 0 in label_class:
+            label_class.remove(0)
+        if 255 in label_class:
+            label_class.remove(255)
 
-            label_class = new_label_class  # [13, 15]
-            # 在找当前训练集对应类的图片
-            if len(label_class) > 0:  # 判断该图片是否有类满足上述条件,有就加入LIST中
-                image_label_list.append(item)  # 共4760张
-                for c in label_class:  # [13, 15]
-                    if c in sub_list:  # [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-                        sub_class_file_list[c].append(item)  # 如果该类在训练类中,就加入训练集对应的类
-    else:
-        for l_idx in tqdm(range(len(list_read))):  # 5953张图片
-            line = list_read[l_idx]  # 'JPEGImages/2008_000008.jpg SegmentationClassAug/2008_000008.png'
-            line = line.strip()  #
-            line_split = line.split(' ')
-            # for pascal
-            image_name = os.path.join(data_root, line_split[
-                0])  # '/home/prlab/willow/Oneshot/data/VOCdevkit2012/VOC2012/JPEGImages/2008_000008.jpg'
-            label_name = os.path.join(data_root, 'SegmentationClassAug' + line_split[1][
-                                                                          -16:])  # '/home/prlab/willow/Oneshot/data/VOCdevkit2012/VOC2012/SegmentationClassAug/2008_000008.png'
-            #for coco
-            # image_name = os.path.join(data_root, 'images/train2014', line_split[
-            #                                                              0][
-            #                                                          -31:])  # '/home/prlab/willow/Oneshot/data/VOCdevkit2012/VOC2012/JPEGImages/2008_000008.jpg'
-            # label_name = os.path.join(data_root, 'annotations/train2014', line_split[
-            #                                                                   1][
-            #                                                               -31:])  # '/home/prlab/willow/Oneshot/data/VOCdevkit2012/VOC2012/SegmentationClassAug/2008_000008.png'
+        new_label_class = []       
+        for c in label_class:
+            if c in sub_list:
+                tmp_label = np.zeros_like(label)
+                target_pix = np.where(label == c)
+                tmp_label[target_pix[0],target_pix[1]] = 1 
+                if tmp_label.sum() >= 2 * 32 * 32:      
+                    new_label_class.append(c)
 
-            item = (image_name, label_name)
-            label = cv2.imread(label_name, cv2.IMREAD_GRAYSCALE)  # (442, 500)
-            label_class = np.unique(label).tolist()  # [0, 13, 15]
+        label_class = new_label_class    
 
-            if 0 in label_class:  # [0, 13, 15]
-                label_class.remove(0)  # [13, 15],去掉背景
-            if 255 in label_class:
-                label_class.remove(255)  # [13, 15]
-            # 先剔除小目标
-            new_label_class = []
-            for c in label_class:  # [13, 15]
-                if c in sub_list:  ##[6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-                    tmp_label = np.zeros_like(label)  # (442, 500)
-                    target_pix = np.where(label == c)  # 找到对应类的位置信息
-                    tmp_label[target_pix[0], target_pix[1]] = 1  # 将对应位置设置为1
-                    if tmp_label.sum() >= 2 * 32 * 32:  # 判断总的个数是否满足条件,相当于排除小目标
-                        new_label_class.append(c)
-
-            label_class = new_label_class  # [13, 15]
-            # 在找当前训练集对应类的图片
-            if len(label_class) > 0:  # 判断该图片是否有类满足上述条件,有就加入LIST中
-                image_label_list.append(item)  # 共4760张
-                for c in label_class:  # [13, 15]
-                    if c in sub_list:  # [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-                        sub_class_file_list[c].append(item)  # 如果该类在训练类中,就加入训练集对应的类
+        if len(label_class) > 0:
+            image_label_list.append(item)
+            for c in label_class:
+                if c in sub_list:
+                    sub_class_file_list[c].append(item)
                     
     print("Checking image&label pair {} list done! ".format(split))
-    return image_label_list, sub_class_file_list#对应总的满足条件的图片共4760张,sub_class_file_list对应的是一个字典,表示的是当前训练集所对应类的图片
+    return image_label_list, sub_class_file_list
 
 
 
@@ -137,19 +83,19 @@ class SemData(Dataset):
         self.data_root = data_root   
 
         if not use_coco:
-            self.class_list = list(range(1, 21)) #[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+            self.class_list = list(range(1, 21))
             if self.split == 3: 
-                self.sub_list = list(range(1, 16)) #[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-                self.sub_val_list = list(range(16, 21)) #[16,17,18,19,20]
+                self.sub_list = list(range(1, 16))
+                self.sub_val_list = list(range(16, 21))
             elif self.split == 2:
-                self.sub_list = list(range(1, 11)) + list(range(16, 21)) #[1,2,3,4,5,6,7,8,9,10,16,17,18,19,20]
-                self.sub_val_list = list(range(11, 16)) #[11,12,13,14,15]
+                self.sub_list = list(range(1, 11)) + list(range(16, 21))
+                self.sub_val_list = list(range(11, 16))
             elif self.split == 1:
-                self.sub_list = list(range(1, 6)) + list(range(11, 21)) #[1,2,3,4,5,11,12,13,14,15,16,17,18,19,20]
-                self.sub_val_list = list(range(6, 11)) #[6,7,8,9,10]
+                self.sub_list = list(range(1, 6)) + list(range(11, 21))
+                self.sub_val_list = list(range(6, 11))
             elif self.split == 0:
-                self.sub_list = list(range(6, 21)) #[6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-                self.sub_val_list = list(range(1, 6)) #[1,2,3,4,5]
+                self.sub_list = list(range(6, 21))
+                self.sub_val_list = list(range(1, 6))
 
         else:
             if use_split_coco:
@@ -208,26 +154,26 @@ class SemData(Dataset):
 
         if image.shape[0] != label.shape[0] or image.shape[1] != label.shape[1]:
             raise (RuntimeError("Query Image & label shape mismatch: " + image_path + " " + label_path + "\n"))          
-        label_class = np.unique(label).tolist()#[0, 9, 11, 255]
+        label_class = np.unique(label).tolist()
         if 0 in label_class:
             label_class.remove(0)
         if 255 in label_class:
             label_class.remove(255) 
         new_label_class = []       
-        for c in label_class:#[9, 11]
-            if c in self.sub_val_list:#[11, 12, 13, 14, 15]
+        for c in label_class:
+            if c in self.sub_val_list:
                 if self.mode == 'val' or self.mode == 'test':
                     new_label_class.append(c)
             if c in self.sub_list:
                 if self.mode == 'train':
                     new_label_class.append(c)
-        label_class = new_label_class#[11]
+        label_class = new_label_class    
         assert len(label_class) > 0
 
 
-        class_chosen = label_class[random.randint(1,len(label_class))-1]#11
+        class_chosen = label_class[random.randint(1,len(label_class))-1]
         class_chosen = class_chosen
-        target_pix = np.where(label == class_chosen)#11
+        target_pix = np.where(label == class_chosen)
         ignore_pix = np.where(label == 255)
         label[:,:] = 0
         if target_pix[0].shape[0] > 0:
@@ -235,14 +181,14 @@ class SemData(Dataset):
         label[ignore_pix[0],ignore_pix[1]] = 255           
 
 
-        file_class_chosen = self.sub_class_file_list[class_chosen]#11
-        num_file = len(file_class_chosen)#第11类共含有73张图片
+        file_class_chosen = self.sub_class_file_list[class_chosen]
+        num_file = len(file_class_chosen)
 
         support_image_path_list = []
         support_label_path_list = []
         support_idx_list = []
         for k in range(self.shot):
-            support_idx = random.randint(1,num_file)-1#从第11类的73张中选择一张作为支持集
+            support_idx = random.randint(1,num_file)-1
             support_image_path = image_path
             support_label_path = label_path
             while((support_image_path == image_path and support_label_path == label_path) or support_idx in support_idx_list):
